47bf3a44bfb1287ab27cb005be91534e62a4a495
Lucas Fonseca <lfonseca@efuse.io>
2023-11-06 08:03:44 -0500

    add jest and swap to yarn

---
+++ app/lib/actions.test.ts
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('place holder', () => {
+  it('true', () => {
+    expect(true).toBe(true);
+  })
+});
